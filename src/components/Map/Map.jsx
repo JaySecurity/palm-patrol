a08b21b9bff8b5e890e84fcc90722ce3888bc515
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-10 19:28:54 -0400

    add geosearch to map (experimental)

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,33 @@
 import { Icon } from 'leaflet';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
-const theftPin = new Icon({
-  iconUrl: '/images/red-pin.png',
-  iconSize: [40, 50],
-});
+function Leaflet(props) {
+  const [address, setAddress] = useState('');
+  const [markers, setMarkers] = useState([]);
 
-const Leaflet = (props) => {
+  const provider = new OpenStreetMapProvider({
+    params: {
+      email: 'jasonnicholls1981@gmail.com',
+      'accept-language': 'en',
+      countrycodes: 'ca',
+    },
+  });
+  const theftPin = new Icon({
+    iconUrl: '/images/red-pin.png',
+    iconSize: [40, 50],
+  });
+
+  async function handleSearch() {
+    let result = await provider.search({ query: address });
+    result = result[0];
+    console.log(result);
+    const lat = result.y;
+    const long = result.x;
+    setMarkers([...markers, [lat, long]]);
+  }
+
   return (
     <div>
       <MapContainer
@@ -18,14 +39,25 @@ const Leaflet = (props) => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={[42.88652, -79.250854]} icon={theftPin}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {markers.map((marker) => {
+          return (
+            <Marker position={[...marker]} icon={theftPin}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          );
+        })}
       </MapContainer>
+      <input
+        type='text'
+        name='address'
+        onChange={(e) => setAddress(e.target.value)}
+        value={address}
+      />
+      <button onClick={handleSearch}>Search Address </button>
     </div>
   );
-};
+}
 
 export default Leaflet;
