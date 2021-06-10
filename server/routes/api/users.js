351d66db3f3e926e46a927f2f1f8338d64c108d8
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-10 09:43:46 -0400

    add login and verify token controllers

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -3,6 +3,6 @@ const userCtrl = require('../../controllers/api/users');
 
 router.post('/signup', userCtrl.create);
 router.post('/login', userCtrl.login);
-router.post('/verify', userCtrl.verifyToken);
+router.get('/verify', userCtrl.verifyToken);
 
 module.exports = router;
