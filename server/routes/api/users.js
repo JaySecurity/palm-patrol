0dd04ba428354b4eb5e7203aa5d2f202335c1f14
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-09 20:07:24 -0400

    create user signup api route and controller

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const userCtrl = require('../../controllers/api/users');
 
 router.post('/signup', userCtrl.create);
 router.post('/login', userCtrl.login);
