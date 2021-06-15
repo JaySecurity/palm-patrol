adf4d676727cd84052de7e7e381e73d64bbfb5ed
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-15 10:50:48 -0400

    Change signup password comfirm to type password

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,30 +1,30 @@
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import MuiAlert from "@material-ui/lab/Alert";
-import React, { useContext, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { ExitToApp } from "@material-ui/icons";
-import "./SignUp.css";
-import { UserContext } from "../../context/UserContext";
-import axios from "axios";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MuiAlert from '@material-ui/lab/Alert';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ExitToApp } from '@material-ui/icons';
+import './SignUp.css';
+import { UserContext } from '../../context/UserContext';
+import axios from 'axios';
 
 function SignUp() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [location, setLocation] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirm, setConfirm] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [location, setLocation] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
   const [msg, setMsg] = useState(null);
   const history = useHistory();
   const [user, setUser] = useContext(UserContext);
 
   const handleSubmit = async (e) => {
-    console.log("hello");
+    console.log('hello');
     e.preventDefault();
     setMsg(null);
     try {
-      let res = await axios.post("/api/users/signup", {
+      let res = await axios.post('/api/users/signup', {
         firstName,
         lastName,
         email,
@@ -32,12 +32,12 @@ function SignUp() {
         confirm,
       });
       let token = `Bearer ${res.data}`;
-      localStorage.setItem("token", token);
-      res = await axios.post("/api/users/verify", { token });
+      localStorage.setItem('token', token);
+      res = await axios.post('/api/users/verify', { token });
       const validUser = res.data.decoded.user;
       setUser(validUser);
-      history.push("/");
-      console.log("complited signup");
+      history.push('/');
+      console.log('complited signup');
     } catch (e) {
       setUser(null);
       let {
@@ -49,62 +49,62 @@ function SignUp() {
     }
   };
   function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} variant='filled' {...props} />;
   }
   return (
-    <div className="SignUp">
+    <div className='SignUp'>
       <h1>SignUp</h1>
-      {msg && <Alert severity="error">{msg}</Alert>}
-      <form className="SignUp-form" onSubmit={handleSubmit}>
+      {msg && <Alert severity='error'>{msg}</Alert>}
+      <form className='SignUp-form' onSubmit={handleSubmit}>
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="text"
-          id="firstName"
-          name="firstName"
+          type='text'
+          id='firstName'
+          name='firstName'
           value={firstName}
-          label="First Name"
+          label='First Name'
           onChange={(e) => setFirstName(e.target.value)}
         />
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="text"
-          id="lastName"
-          name="lastName"
+          type='text'
+          id='lastName'
+          name='lastName'
           value={lastName}
-          label="Last Name"
+          label='Last Name'
           onChange={(e) => setLastName(e.target.value)}
         />
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="email"
-          id="email"
-          name="email"
+          type='email'
+          id='email'
+          name='email'
           value={email}
-          label="Email"
+          label='Email'
           onChange={(e) => setEmail(e.target.value.toLowerCase())}
         />
 
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="password"
-          id="password"
-          name="password"
+          type='password'
+          id='password'
+          name='password'
           value={password}
-          label="Password"
+          label='Password'
           onChange={(e) => setPassword(e.target.value)}
         />
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="confirm"
-          id="confirm"
-          name="confirm"
+          type='password'
+          id='confirm'
+          name='confirm'
           value={confirm}
-          label="Confirm"
+          label='Confirm'
           onChange={(e) => setConfirm(e.target.value)}
         />
         {/* <TextField
@@ -117,9 +117,9 @@ function SignUp() {
           onChange={(e) => setLocation(e.target.value)} */}
         {/* /> */}
         <Button
-          variant="contained"
-          color="primary"
-          size="small"
+          variant='contained'
+          color='primary'
+          size='small'
           endIcon={<ExitToApp />}
           onClick={handleSubmit}
         >
