99608b96fc445580cc68f72d4c3fd2f7ff7d73f1
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-14 22:51:42 -0400

    setup initial report fetch

--- server/routes/api/reports.js
+++ server/routes/api/reports.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const reportCtrl = require('../../controllers/api/reports');
 const auth = require('../../middleware/auth');
 
-router.get('/', auth, reportCtrl.all);
+router.get('/', reportCtrl.all);
 router.post('/', auth, reportCtrl.create);
 
 module.exports = router;
--- src/components/EventListItem/EventListItem.jsx
+++ src/components/EventListItem/EventListItem.jsx
@@ -1,30 +1,30 @@
-import Avatar from "@material-ui/core/Avatar";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import Collapse from "@material-ui/core/Collapse";
-import { red } from "@material-ui/core/colors";
-import IconButton from "@material-ui/core/IconButton";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
-import React from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Collapse from '@material-ui/core/Collapse';
+import { red } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React from 'react';
 const useStyles = makeStyles((theme) => ({
   root: {},
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: '56.25%', // 16:9
   },
   expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: "rotate(180deg)",
+    transform: 'rotate(180deg)',
   },
   avatar: {
     backgroundColor: red[500],
@@ -43,26 +43,30 @@ function EventListItem({ report }) {
     <Card className={classes.root}>
       <CardHeader
         avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
+          <Avatar aria-label='recipe' className={classes.avatar}>
             R
           </Avatar>
         }
         action={
-          <IconButton aria-label="settings">
+          <IconButton aria-label='settings'>
             <MoreVertIcon />
           </IconButton>
         }
         title={report.title}
-        subheader={report.incidentData}
+        subheader={`${new Date(
+          report.incidentData
+        ).toLocaleDateString()} - ${new Date(
+          report.incidentData
+        ).toLocaleTimeString()}`}
       />
 
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p" noWrap>
+        <Typography variant='body2' color='textSecondary' component='p' noWrap>
           {report.description}
         </Typography>
       </CardContent>
       <CardActions disableSpacing></CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+      <Collapse in={expanded} timeout='auto' unmountOnExit>
         <CardContent></CardContent>
       </Collapse>
     </Card>
