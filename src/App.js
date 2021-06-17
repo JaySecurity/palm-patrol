0cfdc2671cb14bccce127a1b261baf2da627488d
OritLeshem <azmono@gmail.com>
2021-06-17 13:12:10 -0400

    profile

--- src/App.js
+++ src/App.js
@@ -1,38 +1,43 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { BrowserRouter as Browser, Route, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header/Header';
-import Login from './components/Login/Login';
-import Nav from './components/Nav/Nav';
-import ReportCategories from './components/ReportCategories/ReportCategories';
-import ReportDetail from './components/ReportDetail/ReportDetail';
-import SignUp from './components/SignUp/SignUp';
-import AddReportPage from './pages/AddReportPage/AddReportPage';
-import ReportsPage from './pages/ReportsPage/ReportsPage';
+import { BrowserRouter as Browser, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Login from "./components/Login/Login";
+import Nav from "./components/Nav/Nav";
+import ReportCategories from "./components/ReportCategories/ReportCategories";
+import ReportDetail from "./components/ReportDetail/ReportDetail";
+import SignUp from "./components/SignUp/SignUp";
+import AddReportPage from "./pages/AddReportPage/AddReportPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ReportsPage from "./pages/ReportsPage/ReportsPage";
 
 function App() {
   return (
     <Browser>
-      <main className='App'>
+      <main className="App">
         <Header />
-        <div className='container'>
+        <div className="container">
           <Switch>
-            <Route path='/login' render={(props) => <Login {...props} />} />
-            <Route path='/signup' render={(props) => <SignUp {...props} />} />
+            <Route path="/login" render={(props) => <Login {...props} />} />
+            <Route path="/signup" render={(props) => <SignUp {...props} />} />
             <Route
-              path='/report/add'
+              path="/profile"
+              render={(props) => <ProfilePage {...props} />}
+            />
+            <Route
+              path="/report/add"
               render={(props) => <AddReportPage {...props} />}
             />
             <Route
-              path='/report/category'
+              path="/report/category"
               render={(props) => <ReportCategories {...props} />}
             />
             <Route
-              path='/report/:id'
+              path="/report/:id"
               render={(props) => <ReportDetail {...props} />}
             />
-            <Route path='/' exact render={(props) => <ReportsPage />} />
+            <Route path="/" exact render={(props) => <ReportsPage />} />
           </Switch>
           <nav>
             <Nav />
