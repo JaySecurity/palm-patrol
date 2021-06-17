b9cd4a98e63192249bd37e9452c24a360e71b684
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-06-17 11:46:52 -0400

    create Edit Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
+import axios from 'axios';
+import { useContext, useEffect } from 'react';
 import { BrowserRouter as Browser, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
@@ -8,25 +10,47 @@ import Nav from './components/Nav/Nav';
 import ReportCategories from './components/ReportCategories/ReportCategories';
 import ReportDetail from './components/ReportDetail/ReportDetail';
 import SignUp from './components/SignUp/SignUp';
+import { UserContext } from './context/UserContext';
 import AddReportPage from './pages/AddReportPage/AddReportPage';
+import EditReportPage from './pages/EditReportPage/EditReportPage';
 import ReportsPage from './pages/ReportsPage/ReportsPage';
 
 function App() {
+  const [user, setUser] = useContext(UserContext);
+  useEffect(async () => {
+    let token = localStorage.getItem('token');
+    if (token) {
+      try {
+        const res = await axios.post('/api/users/verify', { token });
+        const validUser = res.data.decoded.user;
+        setUser(validUser);
+      } catch (e) {
+        setUser(null);
+      }
+    } else {
+      setUser(null);
+    }
+  }, []);
+
   return (
     <Browser>
       <main className='App'>
         <Header />
         <div className='container'>
           <Switch>
+            <Route
+              path='/report/edit/:id'
+              render={(props) => <EditReportPage {...props} />}
+            />
             <Route path='/login' render={(props) => <Login {...props} />} />
             <Route path='/signup' render={(props) => <SignUp {...props} />} />
             <Route
-              path='/report/add'
-              render={(props) => <AddReportPage {...props} />}
-            />
-            <Route
               path='/report/category'
               render={(props) => <ReportCategories {...props} />}
+            />
+            <Route
+              path='/report/add'
+              render={(props) => <AddReportPage {...props} />}
             />
             <Route
               path='/report/:id'
---
+++ src/pages/EditReportPage/EditReportPage.jsx
@@ -0,0 +1,78 @@
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import ReportForm from '../../components/ReportForm/ReportForm';
+import Spinner from '../../components/Spinner/Spinner';
+import { UserContext } from '../../context/UserContext';
+import './EditReportPage.css';
+
+function EditReportPage(props) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user] = useContext(UserContext);
+  const history = useHistory();
+  // const [files, setFiles] = useState([]);
+  const [report, setReport] = useState({
+    user: user._id,
+    title: '',
+    incidentData: '',
+    category: '',
+    location: {
+      address: '',
+      lat: 0,
+      long: 0,
+    },
+    description: '',
+    photos: [],
+  });
+
+  useEffect(() => {
+    if (!user) history.push('/login');
+  });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(`/api/reports/${props.match.params.id}`);
+      await setReport(res.data);
+      res.data.incidentData = new Date(res.data.incidentData).toGMTString();
+      console.log(res.data.incidentData);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    return;
+  };
+
+  return (
+    <div className='EditReportPage'>
+      {isLoading && <Spinner />}
+      <h1>Edit Report</h1>
+      <ReportForm
+        report={report}
+        setReport={setReport}
+        setIsLoading={setIsLoading}
+      />
+      <Button
+        variant='contained'
+        color='primary'
+        size='small'
+        startIcon={<SaveIcon />}
+        className='save-btn'
+        onClick={handleSubmit}
+      >
+        Save
+      </Button>
+    </div>
+  );
+}
+
+export default EditReportPage;
