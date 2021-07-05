cef18c6664591f486c0bb3b03d30dd4b3d33daad
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-07-04 21:43:55 -0400

    fix login to add verified user to context

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,30 @@
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import { ExitToApp } from "@material-ui/icons";
-import CreateIcon from "@material-ui/icons/Create";
-import MuiAlert from "@material-ui/lab/Alert";
-import axios from "axios";
-import React, { useContext, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { UserContext } from "../../context/UserContext";
-import "./Login.css";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { ExitToApp } from '@material-ui/icons';
+import CreateIcon from '@material-ui/icons/Create';
+import MuiAlert from '@material-ui/lab/Alert';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import './Login.css';
 
 function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [msg, setMsg] = useState(null);
   const history = useHistory();
-  const [, setUser] = useContext(UserContext);
+  const [user, setUser] = useContext(UserContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setMsg(null);
     try {
-      let res = await axios.post("/api/users/login", { email, password });
-      let token = `Bearer ${res.data}`;
-      localStorage.setItem("token", token);
-      history.push("/");
+      let res = await axios.post('/api/users/login', { email, password });
+      let token = `Bearer ${res.data.token}`;
+      localStorage.setItem('token', token);
+      setUser(res.data.user);
+      history.push('/');
     } catch (e) {
       setUser(null);
       let {
@@ -36,51 +37,51 @@ function Login(props) {
   };
 
   function Alert(props) {
-    return <MuiAlert elevation={6} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} variant='filled' {...props} />;
   }
 
   return (
-    <div className="Login">
+    <div className='Login'>
       <h1>Login</h1>
-      {msg && <Alert severity="error">{msg}</Alert>}
-      <form className="login-form" onSubmit={handleSubmit}>
+      {msg && <Alert severity='error'>{msg}</Alert>}
+      <form className='login-form' onSubmit={handleSubmit}>
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="email"
-          id="email"
-          name="email"
+          type='email'
+          id='email'
+          name='email'
           value={email}
-          label="Email"
+          label='Email'
           onChange={(e) => setEmail(e.target.value.toLowerCase())}
         />
         <TextField
-          className="form-group"
+          className='form-group'
           required
-          type="password"
-          id="password"
-          name="password"
+          type='password'
+          id='password'
+          name='password'
           value={password}
-          label="Password"
+          label='Password'
           onChange={(e) => setPassword(e.target.value)}
         />
-        <div id="div-buttons">
+        <div id='div-buttons'>
           <Button
-            variant="contained"
-            color="primary"
-            size="small"
-            id="login-button"
+            variant='contained'
+            color='primary'
+            size='small'
+            id='login-button'
             endIcon={<ExitToApp />}
             onClick={handleSubmit}
           >
             Login
           </Button>
-          <Link to="/signup">
+          <Link to='/signup'>
             <Button
-              variant="contained"
-              color="primary"
-              size="small"
-              id="signup-button"
+              variant='contained'
+              color='primary'
+              size='small'
+              id='signup-button'
               endIcon={<CreateIcon />}
             >
               Sign Up
